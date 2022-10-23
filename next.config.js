192100b9af062a542e6ad62ae434017988368163
endremgs <endremg@stud.ntnu.no>
2022-10-23 15:57:51 +0200

    fix cors

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
   },
+  source: "/api/:path*",
+  headers: [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "*" },
+    {
+      key: "Access-Control-Allow-Methods",
+      value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    },
+    {
+      key: "Access-Control-Allow-Headers",
+      value:
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    },
+  ],
 };
 
 module.exports = nextConfig;
