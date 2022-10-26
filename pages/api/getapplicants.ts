f10c35aebd705a23c14ebd61b6ca427226f82cac
Aksel Skullerud Fosaas <aksel.s.fosaas@gmail.com>
2022-10-26 11:46:45 +0200

    fix?

--- pages/api/getapplicants.ts
+++ pages/api/getapplicants.ts
@@ -3,7 +3,7 @@ import { DBapplicant } from "../../types";
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import prisma from "../../lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Applicant } from "@prisma/client";
+import { Applicant, Prisma } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
