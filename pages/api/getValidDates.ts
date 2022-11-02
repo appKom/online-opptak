c2920eba28415bde476c90cc54a44065e5dea84a
Aksel Skullerud Fosaas <aksel.s.fosaas@gmail.com>
2022-11-02 10:46:14 +0100

    small fix

--- pages/api/getValidDates.ts
+++ pages/api/getValidDates.ts
@@ -1,9 +1,8 @@
 import { ValidDates } from "../../types";
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import prisma from "../../lib/prisma";
+
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Prisma } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
