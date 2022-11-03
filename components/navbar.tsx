26313c6ff4dbbd5afa47d5b9eade557028935a8e
Aksel Skullerud Fosaas <aksel.s.fosaas@gmail.com>
2022-11-03 08:29:39 +0100

    enable use of <a> tag

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,24 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full p-5 text-white" style={{backgroundColor: "rgb(0,84,118)"}}>
-        <img src="https://online.ntnu.no/img/online_logo.svg" className='h-[5rem]' />
-        
-        <nav style={{display: "flex", gap: 40, right: 0}}>
-            <Link href="/form/">For søkere</Link>
-            <Link href="/committee/">For komiteer</Link>
-            <Link href="/applicantoverview/">Oversikt</Link>
-        </nav>
-    </div>
-  )
-}
+    <div
+      className="w-full p-5 text-white"
+      style={{ backgroundColor: "rgb(0,84,118)" }}
+    >
+      <img
+        src="https://online.ntnu.no/img/online_logo.svg"
+        className="h-[5rem]"
+      />
 
-export default Navbar
+      <nav style={{ display: "flex", gap: 40, right: 0 }}>
+        <a href="/form">For søkere</a>
+        <a href="/committee">For komiteer</a>
+        <a href="/applicantoverview">Oversikt</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
