185f77f06e465d17863b163f290d08d45c3a9500
Julian <julianao@stud.ntnu.no>
2023-10-11 11:46:34 +0200

    Cleaned react code

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,62 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+const navElements = [
+  {
+    title: "For søkere",
+    uri: "/form",
+  },
+  {
+    title: "For komiteer",
+    uri: "/committee",
+  },
+  {
+    title: "Oversikt",
+    uri: "/applicantoverview",
+  },
+];
 
 const Navbar = () => {
-  return (
-    <div className="w-full p-5 text-white" style={{backgroundColor: "rgb(0,84,118)"}}>
-        <img src="https://online.ntnu.no/img/online_logo.svg" className='h-[5rem]' />
-        
-        <nav style={{display: "flex", gap: 40, right: 0}}>
-            <Link href="/form/">For søkere</Link>
-            <Link href="/committee/">For komiteer</Link>
-            <Link href="/applicantoverview/">Oversikt</Link>
-        </nav>
-    </div>
-  )
-}
+  const router = useRouter();
 
-export default Navbar
+  const isLinkActive = (uri: string) => {
+    return router.pathname === uri;
+  };
+
+  return (
+    <div className="w-full text--online-white bg-online-darkTeal flex justify-between items-center px-10 py-2">
+      <Link href="/" passHref>
+        <a className={isLinkActive("/") ? "active" : ""}>
+          <Image
+            src="/Online_hvit.svg"
+            width={190}
+            height={50}
+            alt="Online logo"
+            className="cursor-pointer"
+          />
+        </a>
+      </Link>
+
+      <div className="flex gap-10 lg:gap-20">
+        {navElements.map((element) => (
+          <Link key={element.uri} href={element.uri}>
+            <a
+              className={
+                "text-online-white hover:text-online-orange transition-all" +
+                (isLinkActive(element.uri)
+                  ? " text-online-orange font-semibold"
+                  : "")
+              }
+            >
+              {element.title}
+            </a>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,6 @@ const Home: NextPage = (props) => {
   return (
     <div>
       <Navbar />
-      <div className="text-center">
-        <h1>Go to...</h1>
-        <button onClick={() => window.location.href = "form"} className="m-2 inline-block px-7 py-3 bg-[rgb(0,84,118)] text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-[rgba(0,84,118,0.8)] hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out">For studenter</button>
-        <button onClick={() => window.location.href = "committee"} className="m-2 inline-block px-7 py-3 bg-[rgb(0,84,118)] text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-[rgba(0,84,118,0.8)] hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out">For komiteer</button>
-        <button onClick={() => window.location.href = "applicantoverview"} className="m-2 inline-block px-7 py-3 bg-[rgb(0,84,118)] text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-[rgba(0,84,118,0.8)] hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out">Oversikt</button>
-      </div>
     </div>
   );
 };
