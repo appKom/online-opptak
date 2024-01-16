af7d24c58fa5be09f912cd7cb6a445036a2b6571
Julian <julianao@stud.ntnu.no>
2024-01-16 13:34:11 +0100

    deployment fix

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { NextPage } from "next";
 import OpptaksForm from "../components/form/opptaksform";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 
 const Form: NextPage = () => {
   return (
