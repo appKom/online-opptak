cd657d575e9074f54c0abfb9fc638073e84da86d
Julian <julianao@stud.ntnu.no>
2024-02-22 17:23:37 +0100

    Removed old files we dont use anymore

--- services/getValidDates.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import { ValidDates } from "../types";
-export function getValidDates(): Promise<{ dates: ValidDates }> {
-  return axios("/api/getValidDates").then((result) => result.data);
-}
-
-export default getValidDates;
