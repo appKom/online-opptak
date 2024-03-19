47ea26a1b3a7772c851aeb95320ba5875626601b
Julian <julianao@stud.ntnu.no>
2024-03-19 15:33:35 +0100

    Period card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,10 @@ import { useSession } from "next-auth/react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import AuthenticationIllustration from "../components/icons/illustrations/AuthenticationIllustration";
-import Button from "../components/Button";
 import { useEffect, useState } from "react";
-import { formatDateNorwegian } from "../lib/utils/dateUtils";
 import { periodType } from "../lib/types/types";
 import { useRouter } from "next/router";
+import PeriodCard from "../components/PeriodCard";
 
 const Home = () => {
   const { data: session } = useSession();
@@ -53,39 +52,7 @@ const Home = () => {
                 Nåværende søknadsperioder
               </h3>
               {currentPeriods.map((period: periodType, index: number) => (
-                <div
-                  key={index}
-                  className="w-full max-w-md mx-auto break-words bg-white rounded-lg shadow"
-                >
-                  <div className="p-4">
-                    <h3 className="text-xl font-medium text-gray-900">
-                      {period.name}
-                    </h3>
-                    <p className="w-full mt-1 text-gray-500">
-                      {period.description}
-                    </p>
-                    <p className="mt-1 text-sm text-gray-500">
-                      Søknadsperiode:{" "}
-                      {formatDateNorwegian(period.applicationPeriod.start)} -{" "}
-                      {formatDateNorwegian(period.applicationPeriod.end)}
-                    </p>
-                    <p className="mt-1 text-sm text-gray-500">
-                      Intervjuperiode:{" "}
-                      {formatDateNorwegian(period.interviewPeriod.start)} -{" "}
-                      {formatDateNorwegian(period.interviewPeriod.end)}
-                    </p>
-                    <div className="flex justify-center mt-2">
-                      <Button
-                        onClick={() =>
-                          router.push(`/application/${period._id}`)
-                        }
-                        title="Søk nå"
-                        size="small"
-                        color="white"
-                      />
-                    </div>
-                  </div>
-                </div>
+                <PeriodCard key={index} period={period} />
               ))}
             </div>
           )
