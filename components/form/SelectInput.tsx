d0990150ae4f681122dbbc4bb20bb5caf5ca3125
Julian <julianao@stud.ntnu.no>
2024-03-19 15:44:54 +0100

    fixed select error

--- components/form/SelectInput.tsx
+++ components/form/SelectInput.tsx
@@ -8,35 +8,35 @@ interface Props {
 
 const SelectInput = (props: Props) => {
   const handleInputChange = (e: React.BaseSyntheticEvent) => {
-    props.updateInputValues(e.target.value);
+    const value = isNaN(e.target.value)
+      ? e.target.value
+      : Number(e.target.value);
+    props.updateInputValues(value);
   };
+
   return (
     <div className="max-w-xs mx-auto my-6">
       <div className="relative">
-        <div onChange={(e) => handleInputChange(e)}>
-          <select
-            className="appearance-none block w-full px-3 py-1.5 text-base border border-solid border-gray-300 rounded cursor-pointer focus:border-blue-600 focus:outline-none"
-            aria-label="Default select example"
-            required={props.required}
-            id="selectComponent"
-          >
-            {props.defaultValue && (
-              <option value="" disabled selected>
-                {props.defaultValue}
-              </option>
-            )}
-            {props.values.map((option) => {
-              return (
-                <option key={option[1]} value={option[1]}>
-                  {option[0]}
-                </option>
-              );
-            })}
-          </select>
-        </div>
+        <select
+          className="appearance-none block w-full px-3 py-1.5 text-base border border-solid border-gray-300 rounded cursor-pointer focus:border-blue-600 focus:outline-none"
+          aria-label={props.label}
+          required={props.required}
+          id="selectComponent"
+          defaultValue=""
+          onChange={handleInputChange}
+        >
+          <option value="" disabled>
+            Velg trinn
+          </option>
+          {props.values.map((option) => (
+            <option key={option[1]} value={option[1]}>
+              {option[0]}
+            </option>
+          ))}
+        </select>
         <label
           htmlFor="selectComponent"
-          className="absolute z-10 px-1 text-xs text-gray-500 bg-white -top-2  left-2"
+          className="absolute z-10 px-1 text-xs text-gray-500 bg-white -top-2 left-2"
         >
           {props.label}
         </label>
