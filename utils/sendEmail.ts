7d673e76e912816399843f2ed21b6785888956a3
Julian <julianao@stud.ntnu.no>
2024-05-05 13:19:31 +0200

    email test endepunkt

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,31 +1,28 @@
-import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 export default async function sendEmail() {
+  const sesClient = new SESClient({ region: "eu-west-1" });
 
-    const sesClient = new SESClient({region: "eu-west-1"});
-
-    const params = {
-        Source: "appkom@online.ntnu.no",
-        Destination: {
-          ToAddresses: [
-            "sindreeh@stud.ntnu.no",
-          ],
-          CcAddresses: [],
-          BccAddresses: [],
+  const params = {
+    Source: "appkom@online.ntnu.no",
+    Destination: {
+      ToAddresses: ["sindreeh@stud.ntnu.no"],
+      CcAddresses: [],
+      BccAddresses: [],
+    },
+    Message: {
+      Subject: {
+        Data: "Test Email",
+        Charset: "UTF-8",
+      },
+      Body: {
+        Html: {
+          Data: 'This is the html content of the email. Let\'s try a link: <a href="https://online.ntnu.no" target="_blank">Online linjeforening hjemmeside</a>',
+          Charset: "UTF-8",
         },
-        Message: {
-          Subject: {
-            Data: "Test Email",
-            Charset: "UTF-8",
-          },
-          Body: {
-            Html: {
-              Data: "This is the html content of the email. Let's try a link: <a href=\"https://online.ntnu.no\" target=\"_blank\">Online linjeforening hjemmeside</a>",
-              Charset: "UTF-8",
-            },
-          },
-        },
-        ReplyToAddresses: [],
+      },
+    },
+    ReplyToAddresses: [],
   };
 
   const command = new SendEmailCommand(params);
