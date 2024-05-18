c5bd498e8b421c1a23d1c952893f265f6ab34d23
fredrir <fhansteen@gmail.com>
2024-05-18 12:25:43 +0200

    can sort by committee

--- pages/committee/[period-id]/index.tsx
+++ pages/committee/[period-id]/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import NotFound from "../../404";
-import { applicantType } from "../../../lib/types/types";
+import { applicantType, periodType } from "../../../lib/types/types";
 import { useRouter } from "next/router";
 
 const CommitteeApplicantOverView: NextPage = () => {
@@ -16,9 +16,24 @@ const CommitteeApplicantOverView: NextPage = () => {
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
   const periodId = router.query["period-id"] as string;
+  const [committees, setCommittees] = useState<string[] | null>(null);
+  const [period, setPeriod] = useState<periodType | null>(null);
+  const [selectedCommittee, setSelectedCommittee] = useState<string | null>(
+    null
+  );
 
   useEffect(() => {
     if (!session || !periodId) return;
+
+    const fetchPeriod = async () => {
+      try {
+        const res = await fetch(`/api/periods/${periodId}`);
+        const data = await res.json();
+        setPeriod(data.period);
+      } catch (error) {
+        console.error("Failed to fetch interview periods:", error);
+      }
+    };
 
     const fetchApplicants = async () => {
       try {
@@ -45,6 +60,7 @@ const CommitteeApplicantOverView: NextPage = () => {
     };
 
     fetchApplicants();
+    fetchPeriod();
   }, [session, periodId]);
 
   useEffect(() => {
@@ -60,6 +76,18 @@ const CommitteeApplicantOverView: NextPage = () => {
     console.log("Filtered applicants:", filteredApplicants);
   }, [searchQuery, applicants]);
 
+  useEffect(() => {
+    if (period && session) {
+      const userCommittees = session.user!.committees;
+      const periodCommittees = period.committees;
+      console.log(userCommittees, periodCommittees);
+      const filteredCommittees = periodCommittees.filter(
+        (committee) => userCommittees?.includes(committee.toLowerCase())
+      );
+      setCommittees(filteredCommittees);
+    }
+  }, [period, session]);
+
   if (!session || !session.user?.isCommitee) {
     return <NotFound />;
   }
@@ -74,52 +102,73 @@ const CommitteeApplicantOverView: NextPage = () => {
 
   return (
     <div className="flex flex-col items-center">
-      <h2 className="mt-5 mb-6 text-3xl font-bold text-center">{`${applicants.length} Søkere`}</h2>
-      <input
-        type="text"
-        placeholder="Søk etter navn"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className="mb-4 p-2 border border-gray-300 rounded dark:bg-online-darkBlue"
-      />
-      {filteredApplicants.length > 0 ? (
-        <table className="min-w-full border border-collapse border-gray-200 dark:bg-online-darkBlue dark:border-gray-700">
-          <thead>
-            <tr>
-              {["Navn", "Beskrivelse", "Bankom", "Klasse", "Telefon"].map(
-                (header) => (
-                  <th
-                    key={header}
-                    className="p-2 border border-gray-200 dark:border-gray-700"
-                  >
-                    {header}
-                  </th>
-                )
-              )}
-            </tr>
-          </thead>
-          <tbody>
-            {filteredApplicants.map((applicant, index) => (
-              <tr key={index} className="">
-                <td className="p-2 border border-gray-200 dark:border-gray-700">
-                  {applicant.name}
-                </td>
-                <td className="p-2 border border-gray-200 dark:border-gray-700">
-                  {applicant.about}
-                </td>
-                <td className="p-2 border border-gray-200 dark:border-gray-700">
-                  {applicant.bankom}
-                </td>
-                <td className="p-2 border border-gray-200 dark:border-gray-700">
-                  {applicant.grade}
-                </td>
-                <td className="p-2 border border-gray-200 dark:border-gray-700">
-                  {applicant.phone}
-                </td>
-              </tr>
+      <h2 className="mt-5 mb-6 text-3xl font-bold text-center">{`${period?.name}`}</h2>
+      <div className="flex flex-row py-5 pt-10">
+        {committees && (
+          <select
+            className="p-2 border text-black border-gray-300 dark:bg-online-darkBlue dark:text-white dark:border-gray-600"
+            value={selectedCommittee ?? ""}
+            onChange={(e) => setSelectedCommittee(e.target.value)}
+          >
+            <option>Velg</option>
+            {committees.map((committee, index) => (
+              <option key={index} value={committee}>
+                {committee}
+              </option>
             ))}
-          </tbody>
-        </table>
+          </select>
+        )}
+        <input
+          type="text"
+          placeholder="Søk etter navn"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="p-2 ml-5 border text-black border-gray-300 dark:bg-online-darkBlue dark:text-white dark:border-gray-600"
+        />
+      </div>
+      {filteredApplicants.length > 0 ? (
+        <div className="min-w-full px-20 py-10">
+          <table className="min-w-full border border-collapse border-gray-200 dark:bg-online-darkBlue dark:border-gray-700">
+            <thead>
+              <tr>
+                {["Navn", "Beskrivelse", "Bankom", "Klasse", "Telefon"].map(
+                  (header) => (
+                    <th
+                      key={header}
+                      className="p-2 border border-gray-200 dark:border-gray-700"
+                    >
+                      {header}
+                    </th>
+                  )
+                )}
+              </tr>
+            </thead>
+            <tbody>
+              {filteredApplicants.map((applicant, index) => (
+                <tr key={index} className="">
+                  <td className="p-2 border border-gray-200 dark:border-gray-700">
+                    {applicant.name}
+                  </td>
+                  <td className="p-2 border border-gray-200 dark:border-gray-700">
+                    {applicant.about}
+                  </td>
+                  <td className="p-2 border border-gray-200 dark:border-gray-700">
+                    {applicant.bankom}
+                  </td>
+                  <td className="p-2 border border-gray-200 dark:border-gray-700">
+                    {applicant.grade}
+                  </td>
+                  <td className="p-2 border border-gray-200 dark:border-gray-700">
+                    {applicant.phone}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="flex justify-end items-end py-5 px-10">
+            <p className="just">{`${applicants.length} resultater`}</p>
+          </div>
+        </div>
       ) : (
         <p>Fant ingen søkere</p>
       )}
