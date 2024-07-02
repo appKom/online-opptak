f8723549545b93b51882106d2448a96a1d727792
fredrir <fhansteen@gmail.com>
2024-07-02 15:26:29 +0200

    Created send message template

--- components/committee/PlanInterview.tsx
+++ components/committee/PlanInterview.tsx
@@ -1,7 +1,14 @@
+import Button from "../Button";
+import TextAreaInput from "../form/TextAreaInput";
+
 const PlanInterview = () => {
   return (
-    <div>
-      <h1>Plan Interviewdf</h1>
+    <div className="flex flex-col gap-5">
+      <h1 className="font-bold text-3xl">Skriv en egendefinert melding!</h1>
+
+      <TextAreaInput updateInputValues={() => {}} label={""} />
+
+      <Button title={"Send Melding"} color={"blue"} />
     </div>
   );
 };
