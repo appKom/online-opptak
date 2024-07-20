d0e3a20393955faec8fc6d9c131be3ba00e9418e
fredrir <fhansteen@gmail.com>
2024-07-20 22:10:55 +0200

    styling for committee card

--- components/committee/CommitteeCard.tsx
+++ components/committee/CommitteeCard.tsx
@@ -1,5 +1,6 @@
 import router from "next/router";
 import React from "react";
+import Button from "../Button";
 
 interface Props {
   committee: string;
@@ -9,11 +10,13 @@ interface Props {
 const CommitteeCard = ({ committee, link }: Props) => {
   return (
     <button
-      className="w-full max-w-md bg-gray-700"
       onClick={() => router.push(link)}
+      className="relative w-full max-w-md mx-auto break-words border rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:text-white"
     >
-      <div className="flex flex-col items-center justify-center h-full">
-        <h1 className="font-bold text-xl">{committee}</h1>
+      <div className="flex flex-col items-center justify-between h-full p-4">
+        <h3 className="text-xl text-center font-medium text-gray-900 dark:text-online-snowWhite">
+          {committee}
+        </h3>
       </div>
     </button>
   );
