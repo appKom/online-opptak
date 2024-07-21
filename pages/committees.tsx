e3cbf3d701f0a4b6ab638d38c032fe33e0b5db06
Julian <julianao@stud.ntnu.no>
2024-07-21 17:40:51 +0200

    added email

--- pages/committees.tsx
+++ pages/committees.tsx
@@ -44,11 +44,14 @@ const Committees = () => {
                 <div className="flex items-center justify-center w-16 h-16 p-1 mb-4 bg-gray-100 rounded-full lg:h-20 lg:w-20 dark:bg-gray-900">
                   <img src={committee.image?.xs} alt={committee.name_long} />
                 </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">
+                <h3 className="text-xl font-bold dark:text-white">
                   {committee.name_long}{" "}
                   {committee.name_long != committee.name_short &&
                     "(" + committee.name_short + ")"}
                 </h3>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  {committee.email}
+                </p>
                 <p className="text-gray-500 dark:text-gray-400">
                   {committee.application_description || "Ingen beskrivelse :("}
                 </p>
