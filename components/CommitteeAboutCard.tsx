5d815607bc640b8be8f38f59aa4c205d78420bff
Julian <julianao@stud.ntnu.no>
2024-07-23 19:51:10 +0200

    updated badge styling

--- components/CommitteeAboutCard.tsx
+++ components/CommitteeAboutCard.tsx
@@ -18,20 +18,18 @@ const CommitteeAboutCard = ({
         <img
           src={image?.xs || "/Online_svart_o.svg"}
           alt={name_long}
-          className="max-w-full max-h-full bg-white rounded-full p-1"
+          className="max-w-full max-h-full p-1 bg-white rounded-full"
         />
-        {hasPeriod && (
-          <p className="flex-grow text-right">
-            <span className="bg-green-200 text-green-700 p-1 rounded-lg">
-              Har opptak!
-            </span>
-          </p>
-        )}
       </div>
 
-      <h3 className="text-xl font-bold dark:text-white">
-        {name_long} {name_long !== name_short && `(${name_short})`}
-      </h3>
+      <div className="flex items-center gap-2">
+        <h3 className="text-xl font-bold dark:text-white">
+          {name_long} {name_long !== name_short && `(${name_short})`}
+        </h3>
+        {hasPeriod && (
+          <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">Har opptak!</span>
+        )}
+            </div>
       <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">{email}</p>
       <p className="text-gray-500 whitespace-pre-wrap dark:text-gray-400">
         {application_description || "Ingen opptaksbeskrivelse"}
