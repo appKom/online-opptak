b269d13346f11dd61cd33b1e4d870a474f98d599
fredrir <fhansteen@gmail.com>
2024-07-23 18:22:21 +0200

    methods to fetch applicants, periods and committee times

---
+++ algorithm/src/mip_matching/fetch-applicants-and-committees.py
@@ -0,0 +1,76 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from datetime import datetime, timezone
+import os
+
+def main():
+    periods = fetch_periods()
+    
+    
+    #Sjekker om perioden er før intervjutiden og etter søknadstiden, og returnerer søkere og komitétider dersom det er tilfelle
+    for period in periods:
+        periodId = period["_id"]
+        interview_start = datetime.fromisoformat(period["interviewPeriod"]["start"].replace("Z", "+00:00"))
+        application_end = datetime.fromisoformat(period["applicationPeriod"]["end"].replace("Z", "+00:00"))
+        
+        now = datetime.now(timezone.utc)
+        
+        if interview_start < now and application_end > now:
+            applicants = fetch_applicants(periodId)
+            committee_times = fetch_committee_times(periodId)
+            # print(applicants)
+            # print(committee_times)
+            
+            return applicants, committee_times
+        
+
+def connect_to_db(collection_name):
+    load_dotenv()
+    
+    mongo_uri = os.getenv("MONGODB_URI")
+    db_name = os.getenv("DB_NAME")
+
+    client = MongoClient(mongo_uri)
+    
+    db = client[db_name] # type: ignore
+    
+    collection = db[collection_name]
+    
+    return collection, client
+
+def fetch_periods():
+    collection, client = connect_to_db("period")
+    
+    periods = collection.find()
+    
+    periods = list(periods)
+    
+    client.close()
+    
+    return periods
+
+def fetch_applicants(periodId):
+    collection, client = connect_to_db("applicants")
+    
+    applicants = collection.find({"periodId": periodId})
+    
+    applicants = list(applicants)
+    
+    client.close()
+    
+    return applicants
+
+def fetch_committee_times(periodId):
+    collection, client = connect_to_db("committee")
+    
+    committee_times = collection.find({"periodId": periodId})
+    
+    committee_times = list(committee_times)
+    
+    client.close()
+    
+    return committee_times
+
+
+
+
