8d46349ac3bf869e8392dd3685af2e089746b46d
fredrir <fhansteen@gmail.com>
2024-07-24 11:34:39 +0200

    remove DOM console warning for CheckIcon

--- components/icons/icons/CheckIcon.tsx
+++ components/icons/icons/CheckIcon.tsx
@@ -8,9 +8,9 @@ const CheckIcon = ({ className }: { className: string }) => (
   >
     <path
       stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
       d="M1 5.917 5.724 10.5 15 1.5"
     />
   </svg>
