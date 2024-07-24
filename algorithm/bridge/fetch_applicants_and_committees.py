509bf4f74833622854ecbab988e058a6a42a2437
fredrir <fhansteen@gmail.com>
2024-07-24 13:42:21 +0200

    match meetings from DB

--- algorithm/bridge/fetch_applicants_and_committees.py
+++ algorithm/bridge/fetch_applicants_and_committees.py
@@ -3,26 +3,44 @@ from dotenv import load_dotenv
 from datetime import datetime, timezone
 import os
 import certifi
+import itertools
+from typing import TypedDict, List
+import mip
+
+from mip_matching.Committee import Committee
+from mip_matching.TimeInterval import TimeInterval
+from mip_matching.Applicant import Applicant
+from mip_matching.match_meetings import match_meetings, MeetingMatch
 
 def main():
     periods = fetch_periods()
     
-    #Sjekker om perioden er etter søknadstiden og før intervjuslutt og hasSentInterviewtimes er false, og returnerer søkere og komitétider dersom det er tilfelle
     for period in periods:
         periodId = str(period["_id"])
         interview_end = datetime.fromisoformat(period["interviewPeriod"]["end"].replace("Z", "+00:00"))
         application_end = datetime.fromisoformat(period["applicationPeriod"]["end"].replace("Z", "+00:00"))
         
-        
         now = datetime.now(timezone.utc)
 
-        if  application_end > now and period["hasSentInterviewTimes"] == False and interview_end < now:
+        if  (application_end > now and period["hasSentInterviewTimes"] == False and interview_end < now) or period["name"] == "Juli Opptak":
             applicants = fetch_applicants(periodId)
             committee_times = fetch_committee_times(periodId)
-            print(applicants)
-            print(committee_times)
             
-            return applicants, committee_times
+            applicant_objects = create_applicant_objects(applicants)
+            committee_objects = create_committee_objects(committee_times)
+            
+            print(applicant_objects)
+            print(committee_objects)
+            
+            match_result = match_meetings(applicant_objects, committee_objects)
+            
+            send_to_db(match_result)
+            return match_result
+        
+        
+def send_to_db(match_result: MeetingMatch):
+    print("Sending to db")
+    print(match_result)
         
 
 def connect_to_db(collection_name):
@@ -71,6 +89,34 @@ def fetch_committee_times(periodId):
     client.close()
     
     return committee_times
+
+def create_applicant_objects(applicants_data: List[dict]) -> set[Applicant]:
+    applicants = set()
+    for data in applicants_data:
+        applicant = Applicant(name=data['name'])
+        for interval_data in data['selectedTimes']:
+            interval = TimeInterval(
+                start=datetime.fromisoformat(interval_data['start'].replace("Z", "+00:00")),
+                end=datetime.fromisoformat(interval_data['end'].replace("Z", "+00:00"))
+            )
+            applicant.add_interval(interval)
+        applicants.add(applicant)
+    return applicants
+
+def create_committee_objects(committee_data: List[dict]) -> set[Committee]:
+    committees = set()
+    for data in committee_data:
+        committee = Committee(name=data['committee'])
+        for interval_data in data['availabletimes']:
+            interval = TimeInterval(
+                start=datetime.fromisoformat(interval_data['start'].replace("Z", "+00:00")),
+                end=datetime.fromisoformat(interval_data['end'].replace("Z", "+00:00"))
+            )
+            capacity = interval_data.get('capacity', 1)  
+            committee.add_interval(interval, capacity)
+        committees.add(committee)
+    return committees
+
 
 if __name__ == "__main__":
     main()
