72b8c10bd1beede541f6a1dfc9ec81cb4fae2952
Julian <julianao@stud.ntnu.no>
2024-07-25 20:12:25 +0200

    using tanstack to fetch /applicants

--- lib/api/applicantApi.ts
+++ lib/api/applicantApi.ts
@@ -1,9 +1,28 @@
-import { QueryFunctionContext } from '@tanstack/react-query';
+import { QueryFunctionContext } from "@tanstack/react-query";
 
-export const fetchApplicantByPeriodAndId = async (context: QueryFunctionContext) => {
+export const fetchApplicantByPeriodAndId = async (
+  context: QueryFunctionContext
+) => {
   const periodId = context.queryKey[1];
   const applicantId = context.queryKey[2];
-  return fetch(`/api/applicants/${periodId}/${applicantId}`).then(res =>
+  return fetch(`/api/applicants/${periodId}/${applicantId}`).then((res) =>
     res.json()
   );
-}
+};
+
+export const fetchApplicantsByPeriodId = async (
+  context: QueryFunctionContext
+) => {
+  const periodId = context.queryKey[1];
+  return fetch(`/api/applicants/${periodId}`).then((res) => res.json());
+};
+
+export const fetchApplicantsByPeriodIdAndCommittee = async (
+  context: QueryFunctionContext
+) => {
+  const periodId = context.queryKey[1];
+  const committee = context.queryKey[2];
+  return fetch(`/api/committees/applicants/${periodId}/${committee}`).then(
+    (res) => res.json()
+  );
+};
