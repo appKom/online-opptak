89d25052d8ca9199888085743a8edba6dbbbb89f
Julian Ammouche Ottosen <42567826+julian-ao@users.noreply.github.com>
2024-07-26 14:49:23 +0200

    use mutation for creating and deleting periods (#213)

--- lib/api/periodApi.ts
+++ lib/api/periodApi.ts
@@ -1,4 +1,5 @@
 import { QueryFunctionContext } from "@tanstack/react-query";
+import { periodType } from "../types/types";
 
 export const fetchPeriodById = async (context: QueryFunctionContext) => {
   const id = context.queryKey[1];
@@ -7,4 +8,20 @@ export const fetchPeriodById = async (context: QueryFunctionContext) => {
 
 export const fetchPeriods = async () => {
   return fetch(`/api/periods`).then((res) => res.json());
+};
+
+export const deletePeriodById = async (id: string) => {
+  return fetch(`/api/periods/${id}`, {
+    method: "DELETE",
+  });
+};
+
+export const createPeriod = async (period: periodType) => {
+  return fetch(`/api/periods`, {
+    method: "POST",
+    body: JSON.stringify(period),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 };
