729b6938c7dbd7fcc6bc1baa2bc087e85693106f
fredrir <fhansteen@gmail.com>
2024-07-29 19:18:37 +0200

    add API method for sending out interview times

---
+++ pages/api/periods/send-interview-times.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { sendOutInterviewTimes } from "../../../lib/utils/sendInterviewTimes/sendInterviewTimes";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    if (req.method === "POST") {
+      //Vill dette expose en sårbarhet?
+      const result = await sendOutInterviewTimes();
+      if (result === undefined) {
+        throw new Error("An error occurred");
+      }
+      const { error } = result;
+      if (error) throw new Error(error);
+      return res.status(201).json({ message: "Period created successfully" });
+    }
+  } catch {
+    res.status(500).json("An error occurred");
+  }
+
+  res.setHeader("Allow", ["POST"]);
+  res.status(405).end(`Method ${req.method} is not allowed.`);
+};
+
+export default handler;
