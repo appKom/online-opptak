11f8d284db48cb643e9b33b3970c823807bf8701
fredrir <fhansteen@gmail.com>
2024-07-29 11:39:36 +0200

    shorten logic

--- lib/utils/sendInterviewTimes/fetchFunctions.ts
+++ lib/utils/sendInterviewTimes/fetchFunctions.ts
@@ -5,35 +5,6 @@ export const fetchCommitteeEmails = async (): Promise<committeeEmails[]> => {
     const baseUrl =
       "https://old.online.ntnu.no/api/v1/group/online-groups/?page_size=999";
 
-    const excludedCommitteeNames = [
-      "HS",
-      "Komiteledere",
-      "Pangkom",
-      "Fond",
-      "Æresmedlemmer",
-      "Bed&Fagkom",
-      "Bekk_påmeldte",
-      "booking",
-      "Buddy",
-      "CAG",
-      "Eksgruppa",
-      "Eldsterådet",
-      "Ex-Komiteer",
-      "interessegrupper",
-      "ITEX",
-      "ITEX-påmeldte",
-      "kobKom",
-      "Komiteer",
-      "Redaksjonen",
-      "Riddere",
-      "techtalks",
-      "Ex-Hovedstyret",
-      "Tillitsvalgte",
-      "Wiki - Komiteer access permissions",
-      "Wiki - Online edit permissions",
-      "X-Sport",
-    ];
-
     const response = await fetch(baseUrl);
 
     if (!response.ok) {
@@ -42,20 +13,10 @@ export const fetchCommitteeEmails = async (): Promise<committeeEmails[]> => {
 
     const data = await response.json();
 
-    const groups = data.results
-      .filter(
-        (group: { name_short: string }) =>
-          !excludedCommitteeNames.includes(group.name_short)
-      )
-      .map((group: owCommitteeType) => ({
-        name_short: group.name_short,
-        name_long: group.name_long,
-        email: group.email,
-        description_short: group.description_short,
-        description_long: group.description_long,
-        image: group?.image,
-        application_description: group.application_description,
-      }));
+    const groups = data.results.map((group: owCommitteeType) => ({
+      name_short: group.name_short,
+      email: group.email,
+    }));
 
     return groups;
   } catch (error) {
