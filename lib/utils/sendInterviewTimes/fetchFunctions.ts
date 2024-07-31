276b6a02b42074a4141f27a64f55a31330a5be48
fredrir <fhansteen@gmail.com>
2024-07-31 21:49:23 +0200

    shorten fetch function

--- lib/utils/sendInterviewTimes/fetchFunctions.ts
+++ lib/utils/sendInterviewTimes/fetchFunctions.ts
@@ -2,10 +2,9 @@ import { committeeEmails, owCommitteeType } from "../../types/types";
 
 export const fetchCommitteeEmails = async (): Promise<committeeEmails[]> => {
   try {
-    const baseUrl =
-      "https://old.online.ntnu.no/api/v1/group/online-groups/?page_size=999";
-
-    const response = await fetch(baseUrl);
+    const response = await fetch(
+      "https://old.online.ntnu.no/api/v1/group/online-groups/?page_size=999"
+    );
 
     if (!response.ok) {
       throw new Error("Failed to fetch data");
