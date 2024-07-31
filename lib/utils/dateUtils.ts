12a24f438c1061fce3a34d2da76424b12468576a
fredrir <fhansteen@gmail.com>
2024-07-31 21:47:47 +0200

    remove comment

--- lib/utils/dateUtils.ts
+++ lib/utils/dateUtils.ts
@@ -19,7 +19,7 @@ export const formatDateHours = (inputDate: undefined | Date) => {
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
 
-  return `${formatDateNorwegian(inputDate)}, ${hours}:${minutes}`; //  - ${hours}:${minutes}
+  return `${formatDateNorwegian(inputDate)}, ${hours}:${minutes}`;
 };
 
 export const formatDateNorwegian = (inputDate?: Date): string => {
