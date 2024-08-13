0da88681a1668790df7aa39ff7ec1fd5588c6728
Julian <julianao@stud.ntnu.no>
2024-08-13 13:56:32 +0200

    trying stuff

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,31 +1,30 @@
 describe("Test login", () => {
-  it("should display login prompt when user is not logged in", () => {
-    cy.session([Cypress.env("EMAIL"), Cypress.env("PASSWORD")], () => {
-      cy.visit("/");
-      cy.contains("Logg inn").should("exist");
-      cy.contains("Logg inn").click();
+  it("should login and authenticate an API call", () => {
+    cy.login().then(() => {
+      cy.request("http://localhost:3000/api/auth/me").then(({ body: user }) => {
+        expect(user).to.have.property("sub");
+        expect(user).to.have.property("email");
 
-      const auth0Issuer = Cypress.env("AUTH0_ISSUER");
+        const { email, sub } = user;
 
-      cy.origin(
-        auth0Issuer,
-        {
-          args: {
-            email: Cypress.env("EMAIL"),
-            password: Cypress.env("PASSWORD"),
-          },
-        },
-        ({ email, password }) => {
-          cy.get('input[name="username"]').type(email);
-          cy.wait(1000);
-          cy.get('button[data-action-button-primary="true"]').click();
-          cy.wait(1000);
-          cy.get('input[name="password"]').type(password);
-          cy.wait(1000);
-          cy.get('button[data-action-button-primary="true"]').click();
-          cy.wait(1000);
-        }
-      );
+        expect(email).to.equal(credentialsDefault.username);
+        expect(sub).to.be.a("string");
+      });
     });
+  });
+  it("should display login prompt when user is not logged in", () => {
+    cy.clearCookies();
+    cy.visit("/");
+    cy.contains("Logg inn").should("exist");
+    cy.contains("Logg inn").click();
+
+    cy.get('input[name="username"]').type(Cypress.env("EMAIL"));
+    cy.wait(1000);
+    cy.get('button[data-action-button-primary="true"]').click();
+    cy.wait(1000);
+    cy.get('input[name="password"]').type(Cypress.env("PASSWORD"));
+    cy.wait(1000);
+    cy.get('button[data-action-button-primary="true"]').click();
+    cy.wait(1000);
   });
 });
