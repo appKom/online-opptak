0f2fc9b88d6ca1aea160631c16f09ad1b7f20a9a
fredrir <fhansteen@gmail.com>
2024-08-13 17:05:14 +0200

    .

--- lib/utils/dateUtils.ts
+++ lib/utils/dateUtils.ts
@@ -30,7 +30,7 @@ export const formatDateHours = (
 export const formatDateNorwegian = (inputDate?: Date): string => {
   const date = new Date(inputDate || "");
 
-  const day = date.getDate().toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2);
   const monthsNorwegian = [
     "jan",
     "feb",
