7f162611679ddb161113c04d58b2530a55154ad4
fredrir <fhansteen@gmail.com>
2024-08-14 12:53:30 +0200

    .

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
     <div className="w-full py-5 px-5 flex flex-col items-center gap-3 text-online-darkTeal dark:text-white">
       <div>
         Skjedd en feil? Ta kontakt med{" "}
-        <Link
+        <a
           className="font-semibold underline transition-all hover:text-online-orange"
           href="mailto:appkom@online.ntnu.no"
         >
           Appkom
-        </Link>{" "}
+        </a>{" "}
         :)
       </div>
     </div>
