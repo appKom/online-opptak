23c73e6cfdfbb42dd4e55ee3aadfb9477b19d1c3
fredrir <fhansteen@gmail.com>
2024-08-14 16:33:32 +0200

    foreach instead of for loop

--- lib/sendInterviewTimes/formatAndSend.ts
+++ lib/sendInterviewTimes/formatAndSend.ts
@@ -23,43 +23,47 @@ export const formatAndSendEmails = async ({
 }: sendInterviewTimesProps) => {
   try {
     // Send email to each applicant
-    for (const applicant of applicantsToEmail) {
-      const typedApplicant: emailApplicantInterviewType = applicant;
-      const applicantEmail = [typedApplicant.applicantEmail];
-      const subject = `Hei, ${typedApplicant.applicantName}, her er dine intervjutider:`;
+    applicantsToEmail.forEach(
+      async (applicant: emailApplicantInterviewType) => {
+        const typedApplicant: emailApplicantInterviewType = applicant;
+        const applicantEmail = [typedApplicant.applicantEmail];
+        const subject = `Hei, ${typedApplicant.applicantName}, her er dine intervjutider:`;
 
-      const emailBody = formatApplicantInterviewEmail(typedApplicant);
-      const phoneBody = formatInterviewSMS(typedApplicant);
+        const emailBody = formatApplicantInterviewEmail(typedApplicant);
+        const phoneBody = formatInterviewSMS(typedApplicant);
 
-      await sendEmail({
-        toEmails: applicantEmail,
-        // toEmails: ["fhansteen@gmail.com"],
-        subject: subject,
-        htmlContent: emailBody,
-      });
+        await sendEmail({
+          toEmails: applicantEmail,
+          // toEmails: ["fhansteen@gmail.com"],
+          subject: subject,
+          htmlContent: emailBody,
+        });
 
-      let toPhoneNumber = "+47";
-      toPhoneNumber += typedApplicant.applicantPhone;
-      sendSMS(toPhoneNumber, phoneBody);
-    }
+        let toPhoneNumber = "+47";
+        toPhoneNumber += typedApplicant.applicantPhone;
+        sendSMS(toPhoneNumber, phoneBody);
+      }
+    );
 
     // Send email to each committee
-    for (const committee of committeesToEmail) {
-      const typedCommittee: emailCommitteeInterviewType = committee;
-      const committeeEmail = [typedCommittee.committeeEmail];
-      const subject = `${changeDisplayName(
-        typedCommittee.committeeName
-      )} sine intervjutider for ${typedCommittee.period_name}`;
+    committeesToEmail.forEach(
+      async (committee: emailCommitteeInterviewType) => {
+        const typedCommittee: emailCommitteeInterviewType = committee;
+        const committeeEmail = [typedCommittee.committeeEmail];
+        const subject = `${changeDisplayName(
+          typedCommittee.committeeName
+        )} sine intervjutider for ${typedCommittee.period_name}`;
 
-      const emailBody = formatCommitteeInterviewEmail(typedCommittee);
+        const emailBody = formatCommitteeInterviewEmail(typedCommittee);
 
-      await sendEmail({
-        toEmails: committeeEmail,
-        // toEmails: ["fhansteen@gmail.com"],
-        subject: subject,
-        htmlContent: emailBody,
-      });
-    }
+        await sendEmail({
+          toEmails: committeeEmail,
+          // toEmails: ["fhansteen@gmail.com"],
+          subject: subject,
+          htmlContent: emailBody,
+        });
+      }
+    );
   } catch (error) {
     return { error: "Failed to send out interview times" };
   }
