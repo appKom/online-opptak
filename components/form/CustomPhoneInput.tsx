06fdc989fa3c5f902b55261fa27e19615f0acdb1
Julian <julianao@stud.ntnu.no>
2024-08-18 22:59:05 +0200

    styling for darkmode

--- components/form/CustomPhoneInput.tsx
+++ components/form/CustomPhoneInput.tsx
@@ -1,4 +1,5 @@
 import PhoneInput from "react-phone-input-2";
+import { useTheme } from "../../lib/hooks/useTheme";
 
 interface Props {
   updateInputValues: Function;
@@ -6,22 +7,31 @@ interface Props {
   defaultValue?: string;
 }
 
-const CustomPhoneInput = (props: Props) => (
-  <div className="w-full max-w-xs pb-2 mx-auto my-6">
-    <label className="px-1 text-xs text-gray-500 transition bg-white -top-2 left-2 dark:bg-gray-900 dark:text-gray-200">
-      Telefonnummer
-    </label>
-    <PhoneInput
-      country="no"
-      inputStyle={{
-        width: "100%",
-        paddingTop: "0.5rem",
-        paddingBottom: "0.5rem",
-      }}
-      value={props.defaultValue}
-      onChange={(text) => props.updateInputValues(text)}
-    />
-  </div>
-);
+const CustomPhoneInput = (props: Props) => {
+  const theme = useTheme();
+
+  const inputStyle = {
+    width: "100%",
+    paddingTop: "0.5rem",
+    paddingBottom: "0.5rem",
+    backgroundColor: "transparent",
+    color: theme === "dark" ? "#fff" : "#000",
+    borderColor: theme === "dark" ? "#4b5563" : "#d1d5db",
+  };
+
+  return (
+    <div className="w-full max-w-xs pb-2 mx-auto my-6">
+      <label className="px-1 text-xs text-gray-500 transition bg-white -top-2 left-2 dark:bg-gray-900 dark:text-gray-200">
+        Telefonnummer
+      </label>
+      <PhoneInput
+        country="no"
+        inputStyle={inputStyle}
+        value={props.defaultValue}
+        onChange={(text) => props.updateInputValues(text)}
+      />
+    </div>
+  );
+};
 
 export default CustomPhoneInput;
