1b710df448fec326e8e1750d777502423dc280e7
fredrir <fhansteen@gmail.com>
2024-08-18 16:39:15 +0200

    add speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import LoadingPage from "../components/LoadingPage";
 import Signature from "../lib/utils/Signature";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -91,6 +92,8 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
           </QueryClientProvider>
         </SessionHandler>
       </div>
+      <Analytics />
+      <SpeedInsights />
     </SessionProvider>
   );
 }
